c94898ecfceaa16c292a4abc3d4d8139a2cb0c51
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 22:15:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,51 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {
+    "Theo" => {
+      :color => ["purple","grey"]
+      :gender => ["male"]
+      :lives => ["Subway"]
+    },
+    "Peter Jr." => {
+      :color => ["purple", "grey"]
+      :gender => ["male"]
+      :lives => ["Library"]
+    },
+    "Lucky" => {
+      :color => ["purple"]
+      :gender => ["male"]
+      :lives => ["Central Park"]
+    },
+    "Mrs. K" => {
+      :color => ["grey", "white"]
+      :gender => ["female"]
+      :lives => ["Central Park"]
+    },
+    "Queenie" => {
+      :color => ["white", "brown"]
+      :gender => ["female"]
+      :lives => ["Subway"]
+    },
+    "Andrew" => {
+      :color => ["white"]
+      :gender => ["male"]
+      :lives => ["City Hall"]
+    },
+    "Alex" => {
+      :color => ["white", "brown"]
+      :gender => ["male"]
+      :lives => ["Central Park"]
+    }
 end
+
+pigeon_data [:Luca]
+  "Luca" => {
+    :color => ["brown"]
+    :gender => ["male"]
+    :lived => ["Central Park"]
+  },
+pigeon_data [:Lola]
+  "Lola" => {
+    :color => ["black"]
+    :gender => ["female"]
+    :lives => ["Library"]
+  }
