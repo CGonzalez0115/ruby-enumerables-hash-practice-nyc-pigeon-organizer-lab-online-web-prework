506ba3aa9a88cfd5bd83609de63b8294c0b5eef1
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 21:53:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {
+    "Theo" => {
+      :color => ["purple","grey"]
+      :gender => ["male"]
+      :lives => ["Subway"]
+    },
+    
+    :color => {
+      :purple => ["Peter Jr.", "Lucky"],
+      :grey => ["Peter Jr.", "Ms. K"],
+      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+      :brown => ["Queenie", "Alex"]
+    },
+    :gender => {
+      :male => ["Alex", "Peter Jr.", "Andrew", "Lucky"],
+      :female => ["Queenie", "Ms. K"]
+    },
+    :lives => {
+      "Subway" => ["Queenie"],
+      "Central Park" => ["Alex", "Ms. K", "Lucky"],
+      "Library" => ["Peter Jr."],
+      "City Hall" => ["Andrew"]
+    }
+  }  # write your code here!
 end
